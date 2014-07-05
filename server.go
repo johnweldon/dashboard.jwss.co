2d621a9a42f148ea181f36d6882bcd6d4ac693c1
John Weldon <johnweldon4@gmail.com>
2014-07-05 13:58:18 -0700

    add github.com/unrolled/secure

--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
 	"github.com/phyber/negroni-gzip/gzip"
+	"github.com/unrolled/secure"
 
 	"github.com/johnweldon/dashboard.jwss.co/area/test"
 )
@@ -14,14 +15,25 @@ func main() {
 	router := mux.NewRouter()
 	test.RegisterArea(router.PathPrefix("/test").Subrouter())
 
+	secureMiddleware := secure.New(secure.Options{
+		SSLRedirect:        true,
+		SSLHost:            "secure.jwss.co/dashboard",
+		SSLProxyHeaders:    map[string]string{"X-Forwarded-Proto": "https"},
+		FrameDeny:          true,
+		ContentTypeNosniff: true,
+		BrowserXssFilter:   true,
+		IsDevelopment:      false,
+	})
+
 	n := negroni.Classic()
+	n.Use(negroni.HandlerFunc(secureMiddleware.HandlerFuncWithNext))
 	n.Use(gzip.Gzip(gzip.DefaultCompression))
 	n.UseHandler(router)
 	n.Run(listenEndpoint())
 }
 
 func listenEndpoint() string {
-	port := "9191"
+	port := "3000"
 	if p := os.Getenv("PORT"); p != "" {
 		port = p
 	}
